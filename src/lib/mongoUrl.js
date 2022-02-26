192f9502ab838ffe6186a2b388b60448df9ddbcf
Shay Mesh <48048666+ShayMeshGit@users.noreply.github.com>
2022-02-26 14:06:06 +0200

    Updated mongoUrl file

--- src/lib/mongoUrl.js
+++ src/lib/mongoUrl.js
@@ -1,7 +1,2 @@
-const DB_PREFIX = process.env.MONGO_PREFIX;
-const DB_HOST = process.env.MONGO_HOST;
-const DB_PORT = process.env.MONGO_PORT;
-const DB_COLLECTION = process.env.MONGO_COLLECTION;
-const DB_USERNAME = process.env.MONGO_USERNAME;
-const DB_PASSWORD = process.env.MONGO_PASSWORD;
-exports.MONGO_URI = `${DB_PREFIX}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_COLLECTION}?authSource=admin`;
+//Add your mongodb uri here, use process.env to extract user credential
+exports.MONGO_URI = '';
